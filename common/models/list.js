05f93d8202e6219bb6e799969ea9421eb75ba4fa
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-05-28 20:21:22 -0600

    users link to lists

--- common/models/list.js
+++ common/models/list.js
@@ -2,4 +2,14 @@
 
 module.exports = function(List) {
 
+  List.observe('before save', function(ctx, next) {
+    if(ctx.instance){
+       ctx.instance.userId = ctx.options.accessToken.userId;
+    }else if (ctx.data){
+       ctx.data.userId = ctx.options.accessToken.userId;
+    }
+    return next();
+  });
+
+
 };
