0a8ba30f48e65075596f3746dce3f532d753bf1f
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-06-11 17:58:10 -0600

    fix eslint errors

--- common/models/list.js
+++ common/models/list.js
@@ -1,15 +1,12 @@
 'use strict';
 
 module.exports = function(List) {
-
   List.observe('before save', function(ctx, next) {
-    if(ctx.instance){
-       ctx.instance.userId = ctx.options.accessToken.userId;
-    }else if (ctx.data){
-       ctx.data.userId = ctx.options.accessToken.userId;
+    if (ctx.instance) {
+      ctx.instance.userId = ctx.options.accessToken.userId;
+    } else if (ctx.data) {
+      ctx.data.userId = ctx.options.accessToken.userId;
     }
     return next();
   });
-
-
 };
--- test/list.js
+++ test/list.js
@@ -1,29 +1,23 @@
-process.env.NODE_ENV = 'test'
+'use strict';
+process.env.NODE_ENV = 'test';
 
 var request = require('supertest');
 var assert = require('assert');
 var app = require('../server/server');
 
-var errorHandler = require('strong-error-handler');
-console.log(app.get('env'));
-
-
-describe('Lists', function(){
-
-  it('Initially 0 lists', function(done){
-      app.models.List.count({}, function(err, count){
-        assert.deepEqual(count, 0);
-        done();
-      });
+describe('Lists', function() {
+  it('Initially 0 lists', function(done) {
+    app.models.List.count({}, function(err, count) {
+      assert.deepEqual(count, 0);
+      done();
+    });
   });
-
-  it('Doesn\'t allow unauthenticated creation', function(done){
-      request(app).post('/api/lists')
-      .send({
-        name: "test list 1",
-        description: "testing unauthenticated"
-      })
-      .expect(401, done)
+  it('Doesn\'t allow unauthenticated creation', function(done) {
+    request(app).post('/api/lists')
+    .send({
+      name: 'test list 1',
+      description: 'testing unauthenticated',
+    })
+    .expect(401, done);
   });
-
 });
