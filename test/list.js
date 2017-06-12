59e4d83dc31d6b95c2704bd57a82caab7b1efbf8
Russell Perkins <dsw.ninjaboy@gmail.com>
2017-06-11 19:36:20 -0600

    Add multiuser tests

--- test/list.js
+++ test/list.js
@@ -6,18 +6,23 @@ var assert = require('assert');
 var app = require('../server/server');
 
 var user1Auth = '';
+var user1List = '';
+var user2Auth = '';
 
 describe('Lists - Unauthenticated', function() {
+
   it('Initially 0 lists', function(done) {
     app.models.List.count({}, function(err, count) {
       assert.deepEqual(count, 0);
       done();
     });
   });
+
   it('Doesn\'t allow unauthenticated reading', function(done) {
     request(app).get('/api/lists')
     .expect(401, done);
   });
+
   it('Doesn\'t allow unauthenticated list creation', function(done) {
     request(app).post('/api/lists')
     .send({
@@ -29,6 +34,7 @@ describe('Lists - Unauthenticated', function() {
 });
 
 describe('Lists - Authenticated', function() {
+
   it('Register User1', function(done) {
     request(app).post('/api/users')
     .send({
@@ -43,6 +49,7 @@ describe('Lists - Authenticated', function() {
       done();
     });
   });
+
   it('Login User1', function(done) {
     request(app).post('/api/users/login')
     .send({
@@ -56,6 +63,7 @@ describe('Lists - Authenticated', function() {
       done();
     });
   });
+
   it('Create List as User1', function(done) {
     request(app).post('/api/lists?access_token=' + user1Auth)
     .send({
@@ -63,8 +71,14 @@ describe('Lists - Authenticated', function() {
       description: 'testing authenticated',
       access_token: user1Auth,
     })
-    .expect(200, done);
+    .expect(200)
+    .then(response => {
+      assert(response.body.id);
+      user1List = response.body.id;
+      done();
+    });
   });
+
   it('Get Lists as User1', function(done) {
     request(app).get('/api/lists?access_token=' + user1Auth)
     .expect(200)
@@ -73,4 +87,54 @@ describe('Lists - Authenticated', function() {
       done();
     });
   });
+
+  it('Get single list as User1', function(done) {
+    request(app).get('/api/lists/'+ user1List +'?access_token=' + user1Auth)
+    .expect(200)
+    .then(response => {
+      assert(response.body.id);
+      done();
+    });
+  });
+
+});
+
+
+
+describe('Lists - Authenticated Multiuser', function() {
+
+  it('Register User2', function(done) {
+    request(app).post('/api/users')
+    .send({
+      username: 'testuser2',
+      email: 'testuser2@example.org',
+      password: 'test',
+    })
+    .expect(200)
+    .then(function(response) {
+      assert(response.body.email, 'testuser2@example.org');
+      assert(response.body.username, 'testuser2');
+      done();
+    });
+  });
+
+  it('Login User2', function(done) {
+    request(app).post('/api/users/login')
+    .send({
+      username: 'testuser2',
+      password: 'test',
+    })
+    .expect(200)
+    .then(function(response) {
+      assert(response.body.id);
+      user2Auth = response.body.id;
+      done();
+    });
+  });
+
+  it('User2 Can\'t get list of User1 ', function(done) {
+    request(app).get('/api/lists/'+ user1List +'?access_token=' + user2Auth)
+    .expect(401, done);
+  });
+
 });
