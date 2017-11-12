504a262a562c8c1d9e95a3c93ee69a4540ad297b
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2017-11-12 16:43:05 -0700

    GET List - only returns lists owned by the user

--- common/models/list.js
+++ common/models/list.js
@@ -1,6 +1,24 @@
 'use strict';
 
 module.exports = function(List) {
+  // Get Lists
+  List.getLists = function (options, cb) {
+
+    // User Info
+    const token = options && options.accessToken;
+    const userId = token && token.userId;
+    console.log('User ID: %s', userId)
+
+    // Only return lists owned by the user
+    List.find({
+      where: {
+        userId: userId
+      }
+    }, cb);
+
+  };
+
+  // Update
   List.observe('before save', function(ctx, next) {
     if (ctx.instance) {
       ctx.instance.userId = ctx.options.accessToken.userId;
