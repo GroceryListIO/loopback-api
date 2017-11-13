80ff4effaa4008f69e313c8a353c3ac703e35fb3
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2017-11-12 17:35:39 -0700

    fix multi user permissions

--- common/models/item.js
+++ common/models/item.js
@@ -2,4 +2,20 @@
 
 module.exports = function(Item) {
 
+    Item.observe('access', function (ctx, next) {
+        const token = ctx.options && ctx.options.accessToken;
+        const userId = token && token.userId;
+        const user = userId ? 'user#' + userId : '<anonymous>';
+
+        const modelName = ctx.Model.modelName;
+        const scope = ctx.where ? JSON.stringify(ctx.where) : '<all records>';
+        console.log('%s: %s accessed %s:%s', new Date(), user, modelName, scope);
+        if (scope == "<all records>") {
+            // We should verify the user can do such things.
+            console.log("Papers please")
+            console.log(JSON.stringify(ctx))
+        }
+        next();
+    });
+    
 };
--- common/models/list.js
+++ common/models/list.js
@@ -18,6 +18,39 @@ module.exports = function(List) {
 
   };
 
+  List.observe('access', function (ctx, next) {
+    // Verify user can access object
+    // Assume <all records> is handled by its GET endpoint
+    
+    const token = ctx.options && ctx.options.accessToken;
+    const userId = token && token.userId;
+    const user = userId ? 'user#' + userId : '<anonymous>';
+
+    const modelName = ctx.Model.modelName;
+    const scope = ctx.query.where ? JSON.stringify(ctx.query.where) : '<all records>';
+    // console.log('%s: %s accessed %s:%s', new Date(), user, modelName, scope);
+    
+    if (scope != "<all records>") {
+      // Verify user can access specific List
+
+      // If the user is looking up their own ID
+      if (ctx.query.where && ctx.query.where.id == userId) {
+        return next()
+      }
+
+      // If the user is listed as the owner in authorizedRoles
+      if (!ctx.options.authorizedRoles || ctx.options.authorizedRoles.$owner == true ) {
+        return next()
+      }
+
+      const error = new Error("Access To List Denied.");
+      error.status = 401;
+      return next(error);
+    }
+    
+    next();
+  });
+
   // Update
   List.observe('before save', function(ctx, next) {
     if (ctx.instance) {
