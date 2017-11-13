b188882c5b2e0859f979fbe3ddcb426cb146bcbe
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2017-11-12 17:59:38 -0700

    eslint

--- common/models/item.js
+++ common/models/item.js
@@ -1,21 +1,3 @@
 'use strict';
 
-module.exports = function(Item) {
-
-    Item.observe('access', function (ctx, next) {
-        const token = ctx.options && ctx.options.accessToken;
-        const userId = token && token.userId;
-        const user = userId ? 'user#' + userId : '<anonymous>';
-
-        const modelName = ctx.Model.modelName;
-        const scope = ctx.where ? JSON.stringify(ctx.where) : '<all records>';
-        console.log('%s: %s accessed %s:%s', new Date(), user, modelName, scope);
-        if (scope == "<all records>") {
-            // We should verify the user can do such things.
-            console.log("Papers please")
-            console.log(JSON.stringify(ctx))
-        }
-        next();
-    });
-    
-};
+module.exports = function(Item) {};
--- common/models/list.js
+++ common/models/list.js
@@ -2,26 +2,22 @@
 
 module.exports = function(List) {
   // Get Lists
-  List.getLists = function (options, cb) {
-
+  List.getLists = function(options, cb) {
     // User Info
     const token = options && options.accessToken;
     const userId = token && token.userId;
-    console.log('User ID: %s', userId)
-
     // Only return lists owned by the user
     List.find({
       where: {
-        userId: userId
-      }
+        userId: userId,
+      },
     }, cb);
-
   };
 
-  List.observe('access', function (ctx, next) {
+  List.observe('access', function(ctx, next) {
     // Verify user can access object
     // Assume <all records> is handled by its GET endpoint
-    
+
     const token = ctx.options && ctx.options.accessToken;
     const userId = token && token.userId;
     const user = userId ? 'user#' + userId : '<anonymous>';
@@ -29,25 +25,25 @@ module.exports = function(List) {
     const modelName = ctx.Model.modelName;
     const scope = ctx.query.where ? JSON.stringify(ctx.query.where) : '<all records>';
     // console.log('%s: %s accessed %s:%s', new Date(), user, modelName, scope);
-    
-    if (scope != "<all records>") {
+
+    if (scope != '<all records>') {
       // Verify user can access specific List
 
       // If the user is looking up their own ID
       if (ctx.query.where && ctx.query.where.id == userId) {
-        return next()
+        return next();
       }
 
       // If the user is listed as the owner in authorizedRoles
-      if (!ctx.options.authorizedRoles || ctx.options.authorizedRoles.$owner == true ) {
-        return next()
+      if (!ctx.options.authorizedRoles || ctx.options.authorizedRoles.$owner == true) {
+        return next();
       }
 
-      const error = new Error("Access To List Denied.");
+      const error = new Error('Access To List Denied.');
       error.status = 401;
       return next(error);
     }
-    
+
     next();
   });
 
