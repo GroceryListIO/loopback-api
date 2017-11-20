d1dd8fabad2bc01b7027d37a1165a43d6040d952
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2017-11-20 15:46:16 -0700

    generate profile pics from gravatar

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,15 @@
 'use strict';
+const crypto = require('crypto');
 
-module.exports = function(User) {
-
+module.exports = function(user) {
+  // Before Save - Add profile pic
+  user.observe('before save', function(ctx, next) {
+    if (!ctx.instance.profilePic && ctx.instance.email) {
+      let email = ctx.instance.email.trim().toLowerCase();
+      let emailHash = crypto.createHash('md5').update(email).digest('hex');
+      ctx.instance.profilePic = 'https://www.gravatar.com/avatar/' + emailHash;
+      // console.log('Profile pic generated: %s', ctx.instance.profilePic);
+    }
+    return next();
+  });
 };
