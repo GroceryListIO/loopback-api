978d8e3a9f6d027dd030914efe4e5a3c48e2cf0d
TheDigitalNinja <dsw.ninjaboy@gmail.com>
2017-11-24 13:39:38 -0700

    facebook login support

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ var RateLimit = require('express-rate-limit');
 
 var app = module.exports = loopback();
 
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
 // Logging
 if (process.env.NODE_ENV == 'production') {
   console.log('Production Mode: Enabling logging.');
@@ -41,8 +44,8 @@ var tightApiLimiter = new RateLimit({
 app.use(defaultApiLimiter);
 app.use('/api/Users/', tightApiLimiter);
 
+// Start the web server
 app.start = function() {
-  // start the web server
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
@@ -63,3 +66,29 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Load the provider configurations
+var config = {};
+try {
+  config = require('./providers.json');
+} catch (err) {
+  console.error('Please configure your passport strategy in `providers.json`.');
+  console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+  process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
+
+// Set up passport related models
+passportConfigurator.setupModels({
+  userModel: app.models.user,
+  userIdentityModel: app.models.userIdentity,
+  userCredentialModel: app.models.userCredential
+});
+
+// Configure passport strategies for third party auth providers
+for (var s in config) {
+  var c = config[s];
+  c.session = c.session !== false;
+  passportConfigurator.configureProvider(s, c);
+}
